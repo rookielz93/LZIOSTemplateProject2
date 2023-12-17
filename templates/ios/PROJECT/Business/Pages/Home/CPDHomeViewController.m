448aeb058deb7543670dda58ab3e8baa61ed8580
jinshengli <jinshengli@pinduoduo.com>
2023-12-17 17:37:24 +0800

    feat: init project

---
+++ templates/ios/PROJECT/Business/Pages/Home/CPDHomeViewController.m
@@ -0,0 +1,31 @@
+//
+//  CPDHomeViewController.m
+//  PROJECT
+//
+//  Created by PROJECT_OWNER on TODAYS_DATE.
+//  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
+//
+
+#import "CPDHomeViewController.h"
+
+@interface CPDHomeViewController ()
+
+@end
+
+@implementation CPDHomeViewController
+
+- (void)dealloc {
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
